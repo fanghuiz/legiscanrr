fdb93ceb38255d70257e8faa2225265f35021e44
fanghuiz <15682270+fanghuiz@users.noreply.github.com>
2019-08-21 17:26:31 -0400

    Added decode_bill_text() function

---
+++ R/decode_bill_text.R
@@ -0,0 +1,55 @@
+#' Decode bill text from base64 to plain text
+#'
+#' Return a list containing bill text in a character string, and meta data
+#'
+#' @param bill_text object returned from get_bill_text()
+#'
+#' @import jsonlite
+#' @import readtext
+#' @importFrom tibble as_tibble
+#'
+#' @return data frame of 10 columns
+#'
+#' @export
+decode_bill_text <- function(bill_text){
+
+  input_bill_text <- bill_text
+
+  # Find correct file extension
+  if(input_bill_text[["mime"]] == "text/html"){
+    ext <- ".html"
+  }
+
+  if(input_bill_text[["mime"]] == "application/pdf"){
+    ext <- ".pdf"
+  }
+
+  if(input_bill_text[["mime"]] == "application/wpd"){
+    ext <- ".wpd"
+  }
+
+  if(input_bill_text[["mime"]] == "application/doc" |
+     input_bill_text[["mime"]] == "application/docx"){
+    ext <- ".docx"
+  }
+
+  if(input_bill_text[["mime"]] == "application/rtf"){
+    ext <- ".rtf"
+  }
+
+  # Decode base64 to binary
+  text_bin <- jsonlite::base64_dec(input_bill_text[["doc"]])
+
+  # Write binary text to temp file using correct mime type extension
+  tmp <- tempfile(fileext = ext)
+  writeBin(text_bin, tmp)
+
+  # Read in from temp file as plain text
+  text_decode <- readtext::readtext(tmp)
+  text_decode <- text_decoded[["text"]]
+
+  # Return a dataframe with decoded texts added in
+  output_bill_text <- tibble::as_tibble(input_bill_text)
+  output_bill_text$text_decode <- text_decode
+  output_bill_text
+}
